1402a55cfbd4887d2488d98d5ce3688413144349
NutsFrFr <nutsfrfrfr@gmail.com>
2026-09-23 03:42:46 +0530

    login ready frontend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Set this once for the frontend deployment. Do not put secrets here.
 window.APP_CONFIG = {
-  API_BASE_URL: 'http://localhost:8000'
+  API_BASE_URL: 'https://backsr-dy97u28bs-null-pointers18.vercel.app'
 };
